649c77f5fa03c7a0788b4ab7ab13eda283c4b86c
ManojKrB <manojbayyarapu.work@gmail.com>
2024-01-04 17:42:37 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Sidepanel from './components/Sidepanel';
+
+function App() {
+  return (
+    <div className="App">
+     <Sidepanel />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Sidepanel.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Sidepanel.module.css'
+
+const Sidepanel = () => {
+  return (
+    <div className={styles.container}>Sidepanel</div>
+  )
+}
+
+export default Sidepanel
